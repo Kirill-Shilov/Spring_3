693eea3a74372f38fd7e0b4c4b446ac026e9b3fa
noob <noob@noob.noob>
2022-10-07 20:49:54 +0300

    project structure

---
+++ tests/registration.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
